d6764792c2abc443bd48b523a5eeb722d5a5af7b
batrueva <ekaterinabatrueva@gmail.com>
2025-01-31 15:39:50 +0300

    31/01/2025

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-P = int(input("Начальная сумма вклада:"))
-R = int(input("Процент по вкладу:"))
-T = int(input("Количество лет:"))
+# P = int(input("Начальная сумма вклада:"))
+# R = int(input("Процент по вкладу:"))
+# T = int(input("Количество лет:"))
 
-S = (P * R * T) / 100
-print("Начисленные проценты:", S)
+# S = (P * R * T) / 100
+# print("Начисленные проценты:", S)
 
 # T1 = input('Введите температуру в градусах Цельсия:')
 # T2 = (9/5)*(T1) + 32
 # print(T1, 'градусов Цельсия равны', T2 'градусам Фаренгейта')
+s = ''
+S = input("Enter string:")
+S = s.replace()
+text = 'My   text is        There'
+print(text.split())
+result = ' '.join(text.split())
+print(result)  # My text is There
+
+print("Результат:", S)
